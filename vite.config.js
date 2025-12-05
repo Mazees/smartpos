79bffa1414a884840851a56fafb0375dc1733b9b
Mazees <madaadha21@gmail.com>
2025-12-05 23:13:06 +0700

    add: pwa vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import mkcert from "vite-plugin-mkcert";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mkcert()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    mkcert(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 });
