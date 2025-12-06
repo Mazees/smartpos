1044a850e014b3f2e2d860fa95c7e66d816c1b08
Mazees <madaadha21@gmail.com>
2025-12-06 13:03:45 +0700

    fix: width kepotong

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,9 +37,13 @@ const Header = ({ children, title }) => {
           </label>
           <h1 className="poppins-extrabold text-xl ml-2">{title}</h1>
         </nav>
-        <div className="w-full flex-1 flex flex-col min-h-0 pl-4 py-2">
-          <Breadcrumbs />
-          <div className="w-full flex-1 overflow-y-auto pr-4 scrollbar-hide">{children}</div>
+        <div className="w-full flex-1 flex flex-col min-h-0 py-2">
+          <div className="pl-4">
+            <Breadcrumbs />
+          </div>
+          <div className="w-full flex-1 overflow-y-auto px-4 scrollbar-hide">
+            {children}
+          </div>
         </div>
       </div>
 
